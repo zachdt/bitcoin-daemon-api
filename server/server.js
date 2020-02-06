4e431f0499d47fb48f29e3f73622f2714d469a58
Zachary Thielemann <zachdt@gmail.com>
2020-02-06 01:08:05 -0500

    init

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const { exec } = require('child_process')
+
+
+while ('')
+exec('bitcoin-cli getblockcount', (err, stdout) => {
+  if (err) {
+    return
+  }
+  console.log(`stdout: ${stdout}`)
+  console.log(`err: ${err}`)
+})
+
+http.createServer(function (req, res) {
+  res.write()
+  res.end()
+}).listen(8080)
