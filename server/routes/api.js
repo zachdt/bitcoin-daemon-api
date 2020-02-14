da35b85338b5b8aec96c7d7122dd8efe7bceaf73
Zachary Thielemann <zachdt@gmail.com>
2020-02-14 13:23:59 -0500

    server: single arg routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -16,6 +16,7 @@ const headers = {
 router.get("/test", (req, res) => res.json({ msg: "Server Active"}))
 
 
+// VOID RPC METHODS
 router.get("/getblockcount", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblockcount","params":[]}`
   var options = {
@@ -167,5 +168,87 @@ router.get("/getrawmempool", (req, res) => {
   request(options, callback)
 })
 
+// 1 ARG RPC METHODS 
+router.get("/getblockhash/:index", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblock","params":["${
+    req.params.index
+  }"]}`
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  }
+  
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body)
+      res.send(data)
+    }
+  }
+  request(options, callback)
+})
+
+
+router.get("/getblock/:hash", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblock","params":["${
+    req.params.hash
+  }"]}`
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  }
+  
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body)
+      res.send(data)
+    }
+  }
+  request(options, callback)
+})
+
+
+router.get("/getrawtransaction/:id", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getrawtransaction","params":["${
+    req.params.id
+  }"]}`
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  }
+  
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body)
+      res.send(data)
+    }
+  }
+  request(options, callback)
+})
+
+router.get("/decoderawtransaction/:hex", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"decoderawtransaction","params":["${
+    req.params.hex
+  }"]}`
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  }
+  
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body)
+      res.send(data)
+    }
+  }
+  request(options, callback)
+})
 
 module.exports = router
